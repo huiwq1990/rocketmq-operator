a76e83350a9dc443bffaeed24195dc4599644474
huiwq1990 <huiwq1990@163.com>
2020-06-02 16:50:32 +0800

    fix

--- create-operator.sh
+++ create-operator.sh
@@ -19,6 +19,7 @@ set -eux;
 
 # You can change the DOCKERHUB_REPO to your docker repo for development purpose
 DOCKERHUB_REPO="apacherocketmq/rocketmq-operator:0.2.0"
+DOCKERHUB_REPO="huiwq1990/rocketmq-operator:0.2.0"
 # The version of RocketMQ including the Admin Tool
 ROCKETMQ_VERSION="4.5.0"
 
