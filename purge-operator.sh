30c0e71d814734a8b1e60102ca6c17afcf0def01
huiwq1990 <huiwq1990@163.com>
2020-06-03 11:10:17 +0800

    fix

--- purge-operator.sh
+++ purge-operator.sh
@@ -23,6 +23,6 @@ kubectl delete -f deploy/operator.yaml
 kubectl delete -f deploy/role_binding.yaml
 kubectl delete -f deploy/role.yaml
 kubectl delete -f deploy/service_account.yaml
-kubectl delete -f deploy/crds/rocketmq_v1alpha1_broker_crd.yaml
-kubectl delete -f deploy/crds/rocketmq_v1alpha1_nameservice_crd.yaml
-kubectl delete -f deploy/crds/rocketmq_v1alpha1_topictransfer_crd.yaml
+kubectl delete -f deploy/crds/rocketmq.apache.org_brokers_crd.yaml
+kubectl delete -f deploy/crds/rocketmq.apache.org_nameservices_crd.yaml
+kubectl delete -f deploy/crds/rocketmq.apache.org_topictransfers_crd.yaml
